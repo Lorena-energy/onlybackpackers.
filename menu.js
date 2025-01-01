22fbae867649aef49a49e3a131efc6d1e91301b5
Lorena-energy <gerencia@energytelco.es>
2025-01-01 20:25:40 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -105,3 +105,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
