c8c2473f0eefe360f69fc6ff7b0f464d62e07e36
Lorena-energy <gerencia@energytelco.es>
2025-01-03 20:56:02 +0100

    Update auth-interface.js

--- auth-interface.js
+++ auth-interface.js
@@ -1,18 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const registerForm = document.querySelector(".register-form");
-  const loginForm = document.querySelector(".login-form");
-  const registerLink = document.getElementById("register-link");
-  const loginLink = document.getElementById("login-link");
+  const registerForm = document.getElementById("register-form"); // Formulario de registro
+  const loginForm = document.getElementById("login-form"); // Formulario de inicio de sesión
+  const registerLink = document.getElementById("show-register"); // Enlace para mostrar registro
+  const loginLink = document.getElementById("show-login"); // Enlace para mostrar login
 
+  // Mostrar formulario de registro
   registerLink.addEventListener("click", (e) => {
     e.preventDefault();
     loginForm.classList.add("hidden");
     registerForm.classList.remove("hidden");
   });
 
+  // Mostrar formulario de inicio de sesión
   loginLink.addEventListener("click", (e) => {
     e.preventDefault();
     registerForm.classList.add("hidden");
     loginForm.classList.remove("hidden");
   });
 });
+
