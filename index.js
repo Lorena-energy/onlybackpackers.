ec3222962f09312cdb276720a03854b93e249f15
Lorena-energy <gerencia@energytelco.es>
2025-01-03 21:29:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// index.js
-
 // Menú hamburguesa
-document.getElementById("menu-toggle").addEventListener("click", () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menu-toggle");
   const menu = document.getElementById("menu");
-  menu.classList.toggle("active");
+
+  if (menuToggle && menu) {
+    menuToggle.addEventListener("click", () => {
+      menu.classList.toggle("active");
+    });
+  }
 });
-
-// Simulación de autenticación
-const isAuthenticated = false; // Cambia a true después del inicio de sesión.
-
-if (!isAuthenticated && window.location.pathname !== '/login.html') {
-  window.location.href = "login.html"; // Redirige a la página de inicio de sesión si no está autenticado.
-}
-
